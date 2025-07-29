f3f91b48bce128dba6f05079718266b49b264a56
Odete <mangumbalaodetevieira@gmail.com>
2025-07-29 23:15:09 +0100

    resggg

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,9 @@
 export const environment = {
     production: false,
 
-<<<<<<< HEAD
-    apiUrl: 'https://6a85ae15724a.ngrok-free.app'  
-=======
 
     apiUrl: 'https://6a85ae15724a.ngrok-free.app'  
 
 
-
->>>>>>> OdeteVieira
   };
   
