9e6e6440f8d522766ad993d2425256cb3d26efd2
Odete <mangumbalaodetevieira@gmail.com>
2025-07-29 19:42:02 +0100

    depp

--- src/app/services/ver-pauta-secretaria.service.ts
+++ src/app/services/ver-pauta-secretaria.service.ts
@@ -1,4 +1,4 @@
-/*
+
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
@@ -85,4 +85,3 @@ export class MenuService {
 }
 
 }
-*/
