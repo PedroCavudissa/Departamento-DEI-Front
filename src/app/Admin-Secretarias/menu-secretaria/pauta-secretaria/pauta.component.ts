3dfaf75bc6ca9d8e479ec87cba4bfc0ff31066f8
Odete <mangumbalaodetevieira@gmail.com>
2025-07-29 21:40:55 +0100

    gggg

--- src/app/Admin-Secretarias/menu-secretaria/pauta-secretaria/pauta.component.ts
+++ src/app/Admin-Secretarias/menu-secretaria/pauta-secretaria/pauta.component.ts
@@ -1,7 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { BarralateralComponent } from '../../barralateral/barralateral.component';
+//import { BarralateralComponent } from '../../barralateral/barralateral.component';
 import { CommonModule } from '@angular/common';
 import { AlunoPauta, MenuService, Disciplina } from '../../../services/ver-pauta-secretaria.service';
 import { FormsModule } from '@angular/forms';
