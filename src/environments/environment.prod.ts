1091e0b7de1035193ed9661917579573631e515c
Pedro Cavudissa <pedrojosecavudissaao@gmail.com>
2025-08-02 21:01:25 +0100

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-
-
-    apiUrl: 'https://afc975230978.ngrok-free.app'  
-
-}
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-
-
-    apiUrl: 'https://afc975230978.ngrok-free.app'  
-
-  };
-  
